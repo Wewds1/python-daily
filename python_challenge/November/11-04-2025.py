8d5c01d9d0053eae7705a316c69e319a0922e786
Wewds1 <daveallenborja@gmail.com>
2025-11-04 22:23:32 +0800

    nov 4 tues, image search -> this is easy i just dont know if there is more good way for this

---
+++ python_challenge/November/11-04-2025.py
@@ -0,0 +1,27 @@
+"""
+Image Search
+On November 4th, 2001, Google launched its image search, allowing people to find images using search terms. In this challenge, you will imitate the image search.
+
+Given an array of image names and a search term, return an array of image names containing the search term.
+
+Ignore the case when matching the search terms.
+Return the images in the same order they appear in the input array.
+"""
+
+
+
+def image_search(images, term):
+
+    sub = term.lower()
+    match = []
+
+    for image in images:
+        if sub in image.lower():
+            match.append(image)
+
+    return match
+
+
+
+
+print(image_search(["Sunset.jpg", "Beach.png", "sunflower.jpeg"], "sun"))
