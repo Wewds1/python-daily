6c90d4446983504a02f5cea66cb646d548daf583
Wewds1 <daveallenborja@gmail.com>
2025-11-08 00:55:40 +0800

    nov 7, card combination -> this is so hard! but i enjoyed it had to review recursive

--- python_challenge/November/11-07-2025.py
+++ python_challenge/November/11-07-2025.py
@@ -1,29 +1,9 @@
-import math
+"""
+Counting Cards
+A standard deck of playing cards has 13 unique cards in each suit. Given an integer representing the number of cards to pick from the deck, return the number of unique combinations of cards you can pick.
 
-def unique_card_combinations(k_cards_to_pick):
-    """
-    Calculates the number of unique combinations when picking k cards 
-    from a standard 52-card deck, where order does not matter.
-    """
-    # Total number of cards in the deck
-    n = 52
-    
-    if k_cards_to_pick < 0 or k_cards_to_pick > n:
-        return "Error: Number of cards to pick must be between 0 and 52."
-    
-    # Use the math.comb function to calculate C(52, k)
-    # This is mathematically equivalent to (52!) / (k! * (52-k)!)
-    combinations = math.comb(n, k_cards_to_pick)
-    
-    return combinations
+Order does not matter. Picking card A then card B is the same as picking card B then card A.
+For example, given 52, return 1. There's only one combination of 52 cards to pick from a 52 card deck. And given 2, return 1326, There's 1326 card combinations you can end up with when picking 2 cards from the deck.
 
-# --- Examples ---
-k1 = 52
-k2 = 2
 
-print(f"Number of combinations for picking {k1} cards: {unique_card_combinations(k1)}")
-
-print(f"Number of combinations for picking {k2} cards: {unique_card_combinations(k2)}") 
-# C(52, 2) = (52*51) / (2*1) = 2652 / 2 = 1326
-
-print(f"Number of combinations for picking 5 cards (a poker hand): {unique_card_combinations(5)}")
+"""
