13a4f2a655d60cf9ef53718bc0a8ea86f73a588f
Wewds1 <daveallenborja@gmail.com>
2025-11-09 16:00:31 +0800

    nov 8, post len -> very easy idk

---
+++ python_challenge/November/11-08-2025.py
@@ -0,0 +1,24 @@
+"""
+In this challenge, you are given a string and need to determine if it fits in a social media post. Return the following strings based on the rules given:
+
+"short post" if it fits within a 40-character limit.
+"long post" if it's greater than 40 characters and fits within an 80-character limit.
+"invalid post" if it's too long to fit within either limit.
+
+"""
+
+def can_post(message):
+
+    if len(message) < 40:
+        return "short post"
+
+    elif 40 < len(message) <  80:
+        return "long post"
+
+    else:
+        return "invalid post"
+
+print(can_post("Hello world"))
+
+
+
