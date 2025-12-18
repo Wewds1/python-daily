0b371fa725a5facf7fcf107325de723e0c2d0c31
Wewds1 <daveallenborja@gmail.com>
2025-12-18 21:41:02 +0800

    fuck should have done this

---
+++ python_challenge/December/dec18.py
@@ -0,0 +1,35 @@
+"""
+Checkerboard
+Given an array with two numbers, the first being the number of rows and the second being the number of columns, return a matrix (an array of arrays) filled with "X" and "O" characters of the given size.
+
+The characters should alternate like a checkerboard.
+The top-left cell must always be "X".
+For example, given [3, 3], return:
+
+[
+  ["X", "O", "X"],
+  ["O", "X", "O"],
+  ["X", "O", "X"]
+]"""
+
+
+def create_board(dimensions):
+    rows, cols = dimensions
+    board = []
+
+    for r in range(rows):
+        row = []
+        for c in range(cols):
+            if (r + c) % 2 == 0:
+                row.append("X")
+            else:
+                row.append("O")
+        board.append(row)
+
+    return board
+
+            
+    # return [["X" for i in range(row)] for _ in range(col)]
+
+# print(create_board([3,3]))
+print(create_board([6,1]))
